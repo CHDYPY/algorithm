f62a9329b5bdb72c41eaf5b61c7d1511f9a779c5
CHDYPY <57973193+CHDYPY@users.noreply.github.com>
2021-04-05 22:22:26 +0800

    Update 0-1背包问题.cpp
    排错ing

--- 动态规划/0-1背包问题.cpp
+++ 动态规划/0-1背包问题.cpp
@@ -2,6 +2,7 @@
 
 
 #include<iostream>
+#include <iomanip>
 #include<vector>
 
 using namespace std;
@@ -12,25 +13,35 @@ vector<int> val = {15, 9, 8, 63, 4};
 int N = wt.size();
 int W = 25;
 
-vector<vector<int>> dp(N+1, vector<int>(W, 0));
+vector<vector<int>> dp(N+1, vector<int>(W+1, 0));
 //最大价值矩阵，行x表示取wt的前x项，列y表示取容量y，[x, y]表示对于物品集合wt[0:x]，背包容量为y时的最大价值。 
 
 void setdp() {
 	for(int i = 1; i<=N; i++) {
 		for(int j = 1; j<=W; j++) {
 			//容量不够 
-			if(wt[i] > j) {
+			if(wt[i-1] > j) {
 				dp[i][j] = dp[i-1][j]; 
 			} else { //容量够了，看放了大还是不放大 
-				dp[i][j] = dp[i-1][j] > dp[i-1][j-wt[i]]+val[i] ? dp[i-1][j] : dp[i-1][j-wt[i]]+val[i];
+				dp[i][j] = dp[i-1][j] > dp[i-1][j-wt[i-1]]+val[i-1] ? dp[i-1][j] : dp[i-1][j-wt[i-1]]+val[i-1];
 			}
 		}
 	}
 }
 
+void display() {
+	for(int i = 0; i<=N; i++) {
+		for(int j = 0; j<=W; j++) {
+			cout << setiosflags(ios::right) << dp[i][j] << " ";
+		} 
+		cout << endl;
+	}
+} 
+
 
 int main() {
 	setdp();
+	display();
 	cout << dp[N][W] << endl; 
 	return 0;
 }
